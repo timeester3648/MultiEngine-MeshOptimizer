e6c5f6a0360c07c0b486f3afd634ad2be606674b
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 15:06:58 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+include "../../premake/common_premake_defines.lua"
+
+project "MeshOptimizer"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./**.h",
+		"./**.cpp"
+	}
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
